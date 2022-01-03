40e16a045dab7fb7964048d59dca6054c5251d4d
manviny <manol@indinet.es>
2022-01-03 13:42:17 +0100

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 # wget -O - https://raw.githubusercontent.com/manviny/HOY/master/hello.sh | sudo bash
 
-read -p "nombre del dominio" dominio
+read -p 'nombre del dominio: ' dominio
 echo $dominio
